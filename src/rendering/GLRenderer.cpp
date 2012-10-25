fab3950e59237a7494b207a72e5750a1c0b0b9b8
Minh Pham <minh.pham@utexas.edu>
2012-10-25 13:42:13 -0500

    Added interface code for OpenGL Renderer.

---
+++ src/rendering/GLRenderer.cpp
@@ -0,0 +1,13 @@
+/**
+ * Implement rendering operations using OpenGL backend for various shapes
+ */
+#include <rendering/GLRenderer.h>
+
+#include <GL/gl.h>
+#include <shapes/triangle.h>
+
+using ishi::Triangle;
+
+void Render(Triangle t) {
+
+}
---
+++ src/rendering/GLRenderer.h
@@ -0,0 +1,8 @@
+#ifndef ISHI_RENDERING_GLRENDERER_H_
+#define ISHI_RENDERING_GLRENDERER_H_
+
+#include <shapes/triangle.h>
+
+void Render(ishi::Triangle t);
+
+#endif
